459fc952f33e463e1cabb7369d322bea31d4a0d5
Lily Finley <TechnoFairyGirl@users.noreply.github.com>
2022-04-08 23:40:52 -0500

    Handle errors (kind of).

--- Program.cs
+++ Program.cs
@@ -46,75 +46,79 @@ namespace SSHBot
 
 			foreach (var host in config.Hosts)
 			{
-				Console.WriteLine($"Connecting to '{host.Hostname}' as '{host.Username}'.");
-
-				var connectionInfo = new ConnectionInfo(host.Hostname, host.Username, host.PrivateKeyFile != null ?
-					(AuthenticationMethod)new PrivateKeyAuthenticationMethod(host.Username, new PrivateKeyFile(host.PrivateKeyFile)) :
-					(AuthenticationMethod)new PasswordAuthenticationMethod(host.Username, host.Password));
-
-				// Disable ECDSA on platforms that don't support it.
-				try { using (var ecdsa = new System.Security.Cryptography.ECDsaCng()) ; }
-				catch (NotImplementedException)
+				try
 				{
-					var algsToRemove = connectionInfo.HostKeyAlgorithms.Keys.Where(algName => algName.StartsWith("ecdsa")).ToArray();
-					foreach (var algName in algsToRemove) connectionInfo.HostKeyAlgorithms.Remove(algName);
-				}
+					Console.WriteLine($"Connecting to '{host.Hostname}' as '{host.Username}'.");
 
-				if (host.UploadFiles != null)
-				{
-					using (var sftp = new SftpClient(connectionInfo))
+					var connectionInfo = new ConnectionInfo(host.Hostname, host.Username, host.PrivateKeyFile != null ?
+						(AuthenticationMethod)new PrivateKeyAuthenticationMethod(host.Username, new PrivateKeyFile(host.PrivateKeyFile)) :
+						(AuthenticationMethod)new PasswordAuthenticationMethod(host.Username, host.Password));
+
+					// Disable ECDSA on platforms that don't support it.
+					try { using (var ecdsa = new System.Security.Cryptography.ECDsaCng()) ; }
+					catch (NotImplementedException)
 					{
-						sftp.Connect();
+						var algsToRemove = connectionInfo.HostKeyAlgorithms.Keys.Where(algName => algName.StartsWith("ecdsa")).ToArray();
+						foreach (var algName in algsToRemove) connectionInfo.HostKeyAlgorithms.Remove(algName);
+					}
 
-						Console.WriteLine($"Connected for uploading files.");
-
-						foreach (var fileTransfer in host.UploadFiles)
+					if (host.UploadFiles != null)
+					{
+						using (var sftp = new SftpClient(connectionInfo))
 						{
-							Console.WriteLine($"Uploading '{fileTransfer.LocalFile}' to '{fileTransfer.RemoteFile}'.");
+							sftp.Connect();
 
-							using (var file = new FileStream(fileTransfer.LocalFile, FileMode.Open, FileAccess.Read))
-								sftp.UploadFile(file, fileTransfer.RemoteFile, true);
+							Console.WriteLine($"Connected for uploading files.");
+
+							foreach (var fileTransfer in host.UploadFiles)
+							{
+								Console.WriteLine($"Uploading '{fileTransfer.LocalFile}' to '{fileTransfer.RemoteFile}'.");
+
+								using (var file = new FileStream(fileTransfer.LocalFile, FileMode.Open, FileAccess.Read))
+									sftp.UploadFile(file, fileTransfer.RemoteFile, true);
+							}
+						}
+					}
+
+					if (host.RunCommands != null)
+					{
+						using (var ssh = new SshClient(connectionInfo))
+						{
+							ssh.Connect();
+
+							Console.WriteLine($"Connected for running commands.");
+
+							foreach (var command in host.RunCommands)
+							{
+								Console.WriteLine($"Running '{command}'.");
+
+								var result = ssh.RunCommand(command);
+
+								Console.Write(result.Result);
+								Console.WriteLine($"Exit status = {result.ExitStatus}");
+							}
+						}
+					}
+
+					if (host.DownloadFiles != null)
+					{
+						using (var sftp = new SftpClient(connectionInfo))
+						{
+							sftp.Connect();
+
+							Console.WriteLine($"Connected for downloading files.");
+
+							foreach (var fileTransfer in host.DownloadFiles)
+							{
+								Console.WriteLine($"Downloading '{fileTransfer.RemoteFile}' to '{fileTransfer.LocalFile}'.");
+
+								using (var file = new FileStream(fileTransfer.LocalFile, FileMode.Create, FileAccess.Write))
+									sftp.DownloadFile(fileTransfer.RemoteFile, file);
+							}
 						}
 					}
 				}
-
-				if (host.RunCommands != null)
-				{
-					using (var ssh = new SshClient(connectionInfo))
-					{
-						ssh.Connect();
-
-						Console.WriteLine($"Connected for running commands.");
-
-						foreach (var command in host.RunCommands)
-						{
-							Console.WriteLine($"Running '{command}'.");
-
-							var result = ssh.RunCommand(command);
-
-							Console.Write(result.Result);
-							Console.WriteLine($"Exit status = {result.ExitStatus}");
-						}
-					}
-				}
-
-				if (host.DownloadFiles != null)
-				{
-					using (var sftp = new SftpClient(connectionInfo))
-					{
-						sftp.Connect();
-
-						Console.WriteLine($"Connected for downloading files.");
-
-						foreach (var fileTransfer in host.DownloadFiles)
-						{
-							Console.WriteLine($"Downloading '{fileTransfer.RemoteFile}' to '{fileTransfer.LocalFile}'.");
-
-							using (var file = new FileStream(fileTransfer.LocalFile, FileMode.Create, FileAccess.Write))
-								sftp.DownloadFile(fileTransfer.RemoteFile, file);
-						}
-					}
-				}
+				catch (Exception e) { Console.WriteLine($"Error communicating with '{host.Hostname}'. {e.ToString()}"); }
 			}
 		}
 	}
